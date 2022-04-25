d1505adcf9849f7d864a723a1dfc383d02a591ab
Micah Woods <mwoods@heroku.com>
2022-04-25 10:37:17 -0400

    [NVIM] Move options to their own file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,33 +4,8 @@ if ok then
   impatient.enable_profile()
 end
 
+require("user.options")
 require("user.plugins")
-
---Set highlight on search
-vim.o.hlsearch = true
-
---Make line numbers default
-vim.wo.number = true
-
---Enable mouse mode
-vim.o.mouse = 'a'
-
---Enable break indent
-vim.o.breakindent = true
-
---Save undo history
-vim.opt.undofile = true
-
---Case insensitive searching UNLESS /C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
---Decrease update time
-vim.o.updatetime = 250
-vim.wo.signcolumn = 'yes'
-
---Set colorscheme
-vim.o.termguicolors = true
 
 -- vim.cmd [[colorscheme onedark]]
 local onedarkpro = require('onedarkpro')
