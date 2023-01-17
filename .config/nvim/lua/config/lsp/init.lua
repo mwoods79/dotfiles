ef6e0988fd1714631258a0c0e23675b5429369dd
Micah Woods <micahwoods@gmail.com>
2023-01-16 21:26:48 -0600

    [NVIM] Add formatters to null-ls

--- .config/nvim/lua/config/lsp/init.lua
+++ .config/nvim/lua/config/lsp/init.lua
@@ -170,7 +170,7 @@ if not null_ls_status_ok then
   return
 end
 
--- local formatting = null_ls.builtins.formatting
+local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
@@ -178,16 +178,7 @@ null_ls.setup({
   debug = false,
   sources = {
     diagnostics.credo,
+    formatting.prettier,
+    formatting.rubocop
   },
-  -- on_attach = function(client, bufnr)
-  --   if client.supports_method("textDocument/formatting") then
-  --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-  --     vim.api.nvim_create_autocmd("BufWritePre", {
-  --       group = augroup,
-  --       buffer = bufnr,
-  --       -- on 0.8, you should use vim.lsp.buf.format instead
-  --       callback = vim.lsp.buf.format,
-  --     })
-  --   end
-  -- end,
 })
